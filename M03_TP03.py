bafe728726831510d1757d708893518db52709be
rguyenot2023 <romain.guyenot2023@campus-eni.fr>
2025-07-08 11:08:23 +0200

    "MO3 TP_03"

---
+++ M03_TP03.py
@@ -0,0 +1,11 @@
+total_grains = 0
+grains = 1
+
+for ligne in range(1, 9):
+    for colonne in range(1, 9):
+        print(f"Ligne {ligne}, Colonne {colonne} → {grains} grains")
+        total_grains += grains
+        grains *= 2
+
+print("\n----------------------------------")
+print(f"Total des grains de riz : {total_grains}")
